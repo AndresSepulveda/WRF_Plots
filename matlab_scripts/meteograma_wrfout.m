034f13badba055ded54f2b45c160fadf81a67f98
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2019-08-09 09:09:34 -0400

    Add files via upload

---
+++ matlab_scripts/meteograma_wrfout.m
@@ -0,0 +1,222 @@
+function meteograma_wrfout(dirdata,filename,xx,yy,t1,t2)
+
+% posicion grilla a evaluar
+% xx=[-71]; % longitud
+% yy=[-36]; % latitud
+
+% fechas a evaluar
+% t1={'03-Jan-1979 11:12:00'}; t2={'07-Jan-1979 12:12:00'};
+
+%directorio 
+% dir='/home/chris/Documentos/Toolbox_WRF_matlab';
+dir='/home/matlab/WRF/Toolbox_WRF_matlab';
+
+addpath([dir]);
+addpath([dir,'/funciones']);
+addpath([dir,'/scripts']);
+% addpath([dir,'/funciones/m_map']);
+addpath(['/home/matlab/croco_tools/UTILITIES/m_map1.4h']);
+
+inicio='01-Jan-1979 00:00:00';%Hora de inicio simulacion, por ej. '01-Jan-1979 00:00:00'
+
+d01=[dirdata,filename,'.nc'];
+plotname=['Meteograma',filename];% nombre output
+tname='Meteograma';% titulo de la figura
+
+%% No modificar
+
+XLAT=ncread(d01,'XLAT');%latitud (sur negativo)
+XLONG=ncread(d01,'XLONG');%longitud (oeste negativo)
+
+XLAT=double(XLAT);
+XLAT=squeeze(XLAT(:,:,1));
+
+XLONG=double(XLONG);
+XLONG=squeeze(XLONG(:,:,1));
+
+lat=XLAT(1,:)';
+lon=XLONG(:,1);
+
+x=lon; % x vector arbitrario
+y=lat; % x vector arbitrario
+
+clear minimox positionx minimoy positiony mas_cercanox mas_cercanoy
+for i=1:length(xx)
+[minimox(i),positionx(i)]=min(abs(x-xx(i)));
+[minimoy(i),positiony(i)]=min(abs(y-yy(i)));
+mas_cercanox(i)=x(positionx(i));
+mas_cercanoy(i)=y(positiony(i));
+end
+
+%% Recoleccion data 
+
+myVar='XTIME';% minutos desde iniciada la simulación
+data=ncread(d01,myVar);
+out1 = minutes(diff(datetime([inicio;t1])));
+out2 = minutes(diff(datetime([inicio;t2])));
+
+idx=(floor(data)>=out1 & floor(data)<=out2);
+ts_hour=double(data(idx)./60);
+
+auxnum=find(idx==1);
+auxs=auxnum(1);
+auxe=auxnum(end);
+auxdif=auxe-auxs+1;
+
+myVar='T2';% temperatura a 2 metros
+data=ncread(d01,myVar,[positionx positiony auxs ],[1 1 auxdif]);
+T2=double(data)-273.15;% grados celcius
+T2=squeeze(T2);
+
+myVar='RAINC';% precipitacion esquema cumulos
+data=ncread(d01,myVar,[positionx positiony auxs ],[1 1 auxdif]);
+RAINC=double(data);
+RAINC=squeeze(RAINC);
+
+myVar='RAINNC';% precipitacion convectiva
+data=ncread(d01,myVar,[positionx positiony auxs ],[1 1 auxdif]);
+RAINNC=double(data);
+RAINNC=squeeze(RAINNC);
+
+%%
+
+myVar='PH';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+PH=double(data);
+PH=squeeze(PH);
+
+myVar='PHB';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+PHB=double(data);
+PHB=squeeze(PHB);
+
+myVar='P';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+P=double(data);
+P=squeeze(P);
+
+myVar='PB';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+PB=double(data);
+PB=squeeze(PB);
+
+PT=(P+PB)';
+
+Z=(PH+PHB)./9.81;% altura 
+Z=squeeze(Z)';
+Z(:,1)=[];
+
+myVar='T';% perturbacion temperatura potencial
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+T=double(data)+300;% (theta+t0)
+T=squeeze(T)';
+
+TEMP=T.*((PT./100000).^(2/7));
+TEMPC=TEMP-273.15;% temperatura celcius
+
+myVar='QVAPOR';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf auxdif]);
+QVAPOR=double(data);
+QVAPOR=squeeze(QVAPOR)';
+
+es = 6.112.*exp(17.67.*(TEMP-273.15)./(TEMP-29.65));
+e = QVAPOR .* PT./100./(QVAPOR + 0.622);
+RH = e./es;
+
+zq=500:500:10000;
+for i=1:size(T,1)
+TEMPCn(i,:) = interp1(Z(i,:),TEMPC(i,:),zq);
+RHn(i,:) = interp1(Z(i,:),RH(i,:),zq);
+end
+
+a = datenum(inicio);%'19-Dec-2009 23:00:00' Fecha de referencia
+
+clear hrs out
+for t=1:length(ts_hour)
+hrs=ts_hour(t);%horas a partir de la fecha de referencia
+out{t}=datevec(a+(hrs/24));%vector [año mes dia hora]
+end
+
+for i=1:length(out)
+    D(i,1)=out{i}(1);
+    D(i,2)=out{i}(2);
+    D(i,3)=out{i}(3);
+    D(i,4)=out{i}(4);
+    D(i,5)=out{i}(5);
+    D(i,6)=out{i}(6);
+end
+    
+dt = datetime(D(:,1),D(:,2),D(:,3),D(:,4),D(:,5),D(:,6));
+tt = timetable(dt,T2,RAINC,RAINNC,TEMPCn,RHn);
+TT2 = retime(tt,'hourly','spline');
+
+anios=year(TT2.dt(1:end));
+meses=month(TT2.dt(1:end));
+horas=hour(TT2.dt(1:end));
+% minutos=minute(TT2.dt(1:end));
+%temp=TT2.temp(1:end);
+%q=TT2.q(1:end);
+RAINC=TT2.RAINC(1:end);
+RAINNC=TT2.RAINNC(1:end);
+T2=TT2.T2(1:end);
+%psfc=TT2.psfc(1:end);
+%rain=TT2.rain(1:end);
+% ws=TT2.ws(1:end);
+%wd=TT2.wd(1:end);
+TEMPn=TT2.TEMPn;
+RHn=TT2.RHn;
+
+% TRAIN=RAINC+RAINNC;
+
+CRAINNC=zeros(size(RAINC));
+CRAINC=zeros(size(RAINC));
+% CTRAIN=zeros(size(RAINC));
+
+for i=1:length(RAINC)-1
+CRAINNC(i+1)=RAINNC(i+1)-RAINNC(i);
+CRAINC(i+1)=RAINC(i+1)-RAINC(i);
+% CTRAIN(i+1)=CTRAIN(i+1)-CTRAIN(i);
+end
+
+[X Y]=meshgrid(1:size(TEMPCn,1),zq);
+
+cmap=colormap_cpt('temp-c');
+h=figure;
+subplot(4,2,[1 2 3 4])
+pcolor(X,Y,RHn'.*100)
+shading interp
+hold on
+barra=colorbar('northoutside','FontWeight','bold','Linewidth',1);
+ylabel(barra,' HR [%] ');
+colormap(flipud(cmap(1:1:end,:)));
+caxis([0 100])
+
+[C,h]=contour(X,Y,TEMPCn','k');
+clabel(C,h)
+hold on
+% contourf(X,Y,T(1:20,:)',0,'k','linewidth',2);
+ylabel('Z [m]')
+% xlabel('Horas desde inicio simulación')
+
+subplot(4,2,[5 6])
+plot([0:length(horas)-1],T2,'-or','linewidth',2,'MarkerFaceColor','r')
+grid on
+ylabel('T 2m [°C]')
+% xlabel('Horas desde inicio simulación')
+xlim([min(horas),max(horas)])
+
+hold on
+subplot(4,2,[7 8])
+aux=bar([0:length(horas)-1],[CRAINNC CRAINC],'stacked');
+grid on
+set(aux,{'FaceColor'},{'b';'cyan'});
+ylabel('PP total [mm/hr]')
+xlabel('Horas desde inicio simulación')
+legend(aux,'RAINNC','RAINC')
+xlim([min(horas),max(horas)])
+
+saveas(h,[pwd,'/../figuras/',plotname],'png');
+close all
+
+return
+
---
+++ matlab_scripts/skewt_wrfout.m
@@ -0,0 +1,165 @@
+function skewt_wrfout(dirdata,filename,xx,yy,t1)
+
+% posicion grilla a evaluar
+% xx=[-71]; % longitud
+% yy=[-36]; % latitud
+
+% fecha a evaluar
+% t1={'03-Jan-1979 11:12:00'}
+
+%directorio 
+% dir='/home/chris/Documentos/Toolbox_WRF_matlab';
+dir='/home/matlab/WRF/Toolbox_WRF_matlab';
+
+addpath([dir]);
+addpath([dir,'/funciones']);
+addpath([dir,'/scripts']);
+% addpath([dir,'/funciones/m_map']);
+addpath(['/home/matlab/croco_tools/UTILITIES/m_map1.4h']);
+
+inicio='01-Jan-1979 00:00:00';%Hora de inicio simulacion, por ej. '01-Jan-1979 00:00:00'
+
+d01=[dirdata,filename,'.nc'];
+plotname=['skewt',filename];% nombre output
+tname='skewt';% titulo de la figura
+
+%% No modificar
+
+XLAT=ncread(d01,'XLAT');%latitud (sur negativo)
+XLONG=ncread(d01,'XLONG');%longitud (oeste negativo)
+
+XLAT=double(XLAT);
+XLAT=squeeze(XLAT(:,:,1));
+
+XLONG=double(XLONG);
+XLONG=squeeze(XLONG(:,:,1));
+
+lat=XLAT(1,:)';
+lon=XLONG(:,1);
+
+x=lon; % x vector arbitrario
+y=lat; % x vector arbitrario
+
+clear minimox positionx minimoy positiony mas_cercanox mas_cercanoy
+for i=1:length(xx)
+[minimox(i),positionx(i)]=min(abs(x-xx(i)));
+[minimoy(i),positiony(i)]=min(abs(y-yy(i)));
+mas_cercanox(i)=x(positionx(i));
+mas_cercanoy(i)=y(positiony(i));
+end
+
+clear lat lon mas_cercanox mas_cercanoy minimox minimoy x XLAT XLONG xx y yy 
+
+%% Recoleccion data 
+
+myVar='XTIME';% minutos desde iniciada la simulación
+data=ncread(d01,myVar);
+out1 = minutes(diff(datetime([inicio;t1])));
+
+idx=(floor(data)>=out1);
+
+auxnum=find(idx==1);
+auxs=auxnum(1);
+ts_hour=double(data(auxs)./60);
+
+clear auxnum idx out1 
+
+% myVar='PH';% 
+% data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+% PH=double(data);
+% PH=squeeze(PH);
+% 
+% myVar='PHB';% 
+% data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+% PHB=double(data);
+% PHB=squeeze(PHB);
+
+myVar='P';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+P=double(data);
+P=squeeze(P);
+
+myVar='PB';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+PB=double(data);
+PB=squeeze(PB);
+
+PT=(P+PB);% presion total en Pa
+
+myVar='T';% perturbacion temperatura potencial
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+T=double(data)+300;% (theta+t0)
+T=squeeze(T);% temperatura potencial
+
+TEMP=T.*((PT./100000).^(2/7));% temperatura kelvin
+TEMPC=TEMP-273.15;% temperatura celcius
+
+myVar='QVAPOR';% 
+data=ncread(d01,myVar,[positionx positiony 1 auxs ],[1 1 Inf 1]);
+QVAPOR=double(data);
+QVAPOR=squeeze(QVAPOR);
+
+es = 6.112.*exp(17.67.*(TEMP-273.15)./(TEMP-29.65));
+e = QVAPOR .* PT./100./(QVAPOR + 0.622);
+RH = e./es;% humedad relativa 0-1
+
+pz=PT./100;
+tz=TEMPC;
+rhz=RH;
+
+%%
+ez=6.112.*exp(17.67.*tz./(243.5+tz));
+qz=rhz.*0.622.*ez./(pz-ez);
+chi=log(pz.*qz./(6.112.*(0.622+qz)));
+tdz=243.5.*chi./(17.67-chi);
+%
+p=[1050:-25:100];
+pplot=transpose(p);
+t0=[-48:2:50];
+[ps1,ps2]=size(p);
+ps=max(ps1,ps2);
+[ts1,ts2]=size(t0);
+ts=max(ts1,ts2);
+for i=1:ts,
+   for j=1:ps,
+      tem(i,j)=t0(i)+30.*log(0.001.*p(j));
+      thet(i,j)=(273.15+tem(i,j)).*(1000./p(j)).^.287;
+      es=6.112.*exp(17.67.*tem(i,j)./(243.5+tem(i,j)));
+      q(i,j)=622.*es./(p(j)-es);
+      thetaea(i,j)=thet(i,j).*exp(2.5.*q(i,j)./(tem(i,j)+273.15));
+   end
+end
+p=transpose(p);
+t0=transpose(t0);
+temp=transpose(tem);
+theta=transpose(thet);
+thetae=transpose(thetaea);
+qs=transpose(sqrt(q));
+h=contour(t0,pplot,temp,16,'k');
+hold on
+set(gca,'ytick',[1000:100:100])
+set(gca,'yscale','log','ydir','reverse')
+set(gca,'fontweight','bold')
+set(gca,'ytick',[100:100:1000])
+set(gca,'ygrid','on')
+hold on
+h=contour(t0,pplot,theta,24,'b');
+h=contour(t0,pplot,qs,24,'g');
+h=contour(t0,pplot,thetae,24,'r');
+%tsound=30.+43.5.*log(0.001.*p);
+%tsoundm=tsound-30.*log(0.001.*p);
+tzm=tz-30.*log(0.001.*pz);
+tdzm=tdz-30.*log(0.001.*pz);
+h=plot(tzm,pz,'r',tdzm,pz,'b--');
+set(h,'linewidth',2)
+hold off
+xlabel('Temperatura (°C)','fontweight','bold')
+ylabel('Presión (hPa)','fontweight','bold')
+ylim([100 1000])
+title(tname)
+legend(h,'T parcela','T punto de rocío')
+
+saveas(h,[pwd,'/../figuras/',plotname],'png');
+close all
+
+return
