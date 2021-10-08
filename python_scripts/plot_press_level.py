8879cdbdb439e751a1e972b0643a34274c0d642e
Andres Sepulveda <AndresSepulveda@users.noreply.github.com>
2021-10-08 15:08:05 -0300

    Add files via upload

---
+++ python_scripts/plot_press_level.py
@@ -0,0 +1,73 @@
+from netCDF4 import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+import cartopy.crs as crs
+from cartopy.feature import NaturalEarthFeature
+
+from wrf import (getvar, interplevel, to_np, latlon_coords, get_cartopy,
+                 cartopy_xlim, cartopy_ylim)
+
+# Open the NetCDF file
+ncfile = Dataset("wrfout_d01_2021-09-22_00:00:00")
+
+# Extract the pressure, geopotential height, and wind variables
+p = getvar(ncfile, "pressure")
+z = getvar(ncfile, "z", units="dm")
+ua = getvar(ncfile, "ua", units="m s-1")
+va = getvar(ncfile, "va", units="m s-1")
+wspd = getvar(ncfile, "wspd_wdir", units="m s-1")[0,:]
+
+# Interpolate geopotential height, u, and v winds to 500 hPa
+ht_500 = interplevel(z, p, 500)
+u_500 = interplevel(ua, p, 500)
+v_500 = interplevel(va, p, 500)
+wspd_500 = interplevel(wspd, p, 500)
+
+# Get the lat/lon coordinates
+lats, lons = latlon_coords(ht_500)
+
+# Get the map projection information
+cart_proj = get_cartopy(ht_500)
+
+# Create the figure
+fig = plt.figure(figsize=(12,9))
+ax = plt.axes(projection=cart_proj)
+
+# Download and add the states and coastlines
+#states = NaturalEarthFeature(category="cultural", scale="50m",
+#                             facecolor="none",
+#                             name="admin_1_states_provinces_shp")
+#ax.add_feature(states, linewidth=0.5, edgecolor="black")
+#ax.coastlines('50m', linewidth=0.8)
+
+# Add the 500 hPa geopotential height contours
+levels = np.arange(520., 580., 6.)
+contours = plt.contour(to_np(lons), to_np(lats), to_np(ht_500),
+                       levels=levels, colors="black",
+                       transform=crs.PlateCarree())
+plt.clabel(contours, inline=1, fontsize=10, fmt="%i")
+
+# Add the wind speed contours
+levels = [0, 5, 10, 15 , 20, 25, 30, 35, 40, 45, 50]
+wspd_contours = plt.contourf(to_np(lons), to_np(lats), to_np(wspd_500),
+                             levels=levels,
+                             cmap=get_cmap("rainbow"),
+                             transform=crs.PlateCarree())
+plt.colorbar(wspd_contours, ax=ax, orientation="horizontal", pad=.05)
+
+# Add the 500 hPa wind barbs, only plotting every 125th data point.
+plt.barbs(to_np(lons[::5,::5]), to_np(lats[::5,::5]),
+          to_np(u_500[::5, ::5]), to_np(v_500[::5, ::5]),
+          transform=crs.PlateCarree(), length=6)
+
+# Set the map bounds
+ax.set_xlim(cartopy_xlim(ht_500))
+ax.set_ylim(cartopy_ylim(ht_500))
+
+ax.gridlines()
+
+plt.title("500 MB Height (dm), Wind Speed (m/s), Barbs (m/s)")
+
+#plt.show()
+plt.savefig('plot_ws_500mb_BioBio.png')
